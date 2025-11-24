ac5d3fc3c2b5a90483bec3af601861a88600d544
Computhink-CS143 <cs143@computhink.com.sg>
2025-11-24 08:53:08 +0000

    Nov 24, 2025, 8:53 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -102,7 +102,7 @@ print(name)
 
 title = "Emperor "
 name = "Jaron "
-command = "eat cake"
+command = " eat cake"
 print(title+name+"forces the prisoners to"+command)
 
 # Example Output: Emperor Johnson commands his peasants to eat cake!
