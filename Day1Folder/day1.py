acb6b0afdf9cc55d9d9265a0b9f8df7f8334e9cd
Computhink-CS143 <cs143@computhink.com.sg>
2025-11-24 08:32:29 +0000

    Nov 24, 2025, 8:32 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,6 +1,7 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
 print("hello from day1")
+print("I like cheese")
 
 ########################################################################
 # Task 1:
@@ -9,31 +10,47 @@ print("hello from day1")
 
 ########################################################################
 # Task 2:
-
-
+print("Jaron")
+print("11")
+print("Fuchun Primary School")
+print("A Steam Deck")
 
 ########################################################################
 # Task 3:
-
-
-
+animal1="Weiner Dog"
+animal2="Penguin"
+animal3="Otter"
+print(animal1)
+print(animal2)
+print(animal3)
 ########################################################################
 # Task 4:
-
-
-
+ans=(25+42)
+print(ans)
+variable1=25
+variable2=42
+print(variable1+variable2)
+print(variable1*variable2)
+print(variable1/variable2)
 ########################################################################
 # Task 5:
-
-
-
+name=" My name is Jaron,"
+food="I like Cheese."
+print(name+food)
 ########################################################################
 # Task 6:
-
-
-
+def intro():
+    print("Jaron")
+    print("11")
+    print("Fuchun Primary School")
+    print("A Steam Deck")
+intro()
 ########################################################################
 # Task 7:
+def addition(number1,number2):
+    print(number1+number2)
+    
+addition(54,13)
 
 
 
